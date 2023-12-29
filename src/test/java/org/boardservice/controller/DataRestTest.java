f48ada1496d9d7d126bfc30394899f73faccb107
borussia444423 <jung78978978@naver.com>
2023-12-29 21:32:49 +0900

    #17 - 리팩토링: 사용 안하는 import 정리
    optimize import 완료!

--- src/test/java/org/boardservice/controller/DataRestTest.java
+++ src/test/java/org/boardservice/controller/DataRestTest.java
@@ -8,11 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
