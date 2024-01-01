f80b5057c952f97bac520272a25cd5ff033fef72
borussia444423 <jung78978978@naver.com>
2024-01-01 22:20:32 +0900

    #21 - 뷰 디자인 업데이트 및 기타 업데이트
    * 게시글 본문 가로 스크롤바 안 생기게 wrapping 적용
    * css 인덴트 2 space 로 조정
    * 시간 정보에 `<time>` 태그 적용
    * 본문 태그를 `<pre>`로 변경
    * 검색 페이지, 해시태그 전용 검색 페이지, 회원 가입 페이지 추가
    
    * `ArticleCommentDto` 메서드 추가
    * `ArticleCommentService` 메서드 추가 및 변경
    * `ArticleCommentServiceTest` 테스트 메서드 추가 및 변경

--- src/main/java/org/boardservice/dto/ArticleCommentDto.java
+++ src/main/java/org/boardservice/dto/ArticleCommentDto.java
@@ -1,5 +1,8 @@
 package org.boardservice.dto;
 
+import org.boardservice.domain.Article;
+import org.boardservice.domain.ArticleComment;
+
 import java.time.LocalDateTime;
 
 
@@ -11,6 +14,23 @@ public record ArticleCommentDto(
 		LocalDateTime createdAt,
 		String createdBy,
 		LocalDateTime modifiedAt,
+		String modifiedBy
+) {
+	public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+		return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+	}
+
+	public static ArticleCommentDto from(ArticleComment entity) {
+		return new ArticleCommentDto(
+				entity.getId(),
+				entity.getArticle().getId(),
+				UserAccountDto.from(entity.getUserAccount()),
+				entity.getContent(),
+				entity.getCreatedAt(),
+				entity.getCreatedBy(),
+				entity.getModifiedAt(),
+				entity.getModifiedBy()
+		);
 	}
 
 	public ArticleComment toEntity(Article entity) {
