b748d800cbdfa2a5a03f586f4312ea87ee4819da
borussia444423 <jung78978978@naver.com>
2024-01-03 12:27:03 +0900

    #28 - 페이지네이션 서비스 구현 및 테스트
    Spring Service Bean 형태로 설계하여,
    현재 페이지와 총 페이지 수를 넣어주면
    페이지 바에 사용할 번호 리스트를 반환 되게끔 설계함.

---
+++ src/main/java/org/boardservice/service/PaginationService.java
@@ -0,0 +1,24 @@
+package org.boardservice.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+public class PaginationService {
+
+	private static final int BAR_LENGTH = 5;
+
+
+	public List<Integer> getPaginationBarNumbers(int currentNumber, int totalPages) {
+		int startNumber = Math.max(currentNumber - (BAR_LENGTH / 2), 0);
+		int endNumber = Math.min(startNumber + BAR_LENGTH, totalPages);
+
+		return IntStream.range(startNumber, endNumber).boxed().toList();
+	}
+
+	public int currentBarLength() {
+		return BAR_LENGTH;
+	}
+}
