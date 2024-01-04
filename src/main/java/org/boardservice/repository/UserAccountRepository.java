7456c42343d09d8436e164313cd8ff42dbd05b11
borussia444423 <jung78978978@naver.com>
2024-01-04 12:49:03 +0900

    #42 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바 코드에 반영

--- src/main/java/org/boardservice/repository/UserAccountRepository.java
+++ src/main/java/org/boardservice/repository/UserAccountRepository.java
@@ -3,6 +3,6 @@ package org.boardservice.repository;
 import org.boardservice.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 
 }
